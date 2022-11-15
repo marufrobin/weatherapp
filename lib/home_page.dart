d4d1936da134d74753cc2c9a4bde7a4721060bed
Maruf Robin <maruf.robin@northsouth.edu>
2022-11-15 23:39:37 +0600

    finally location working.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,13 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+  HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+    Position? position;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+    position = await Geolocator.getCurrentPosition();
+    setState(() {
+      lat = position!.latitude;
+      lon = position!.longitude;
+    });
+    print("lattitude: $lat \ Longtitude: $lon");
+  }
+
+  var lat;
+  var lon;
+
+  @override
+  void initState() {
+    determinePosition();
+    // TODO: implement initState
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(child: Scaffold());
